7ad9bc610df0dd0e89e80d934ea6d49f16a5cefe
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-10 01:00:51 +0500

    Created storage service

---
+++ src/appwrite/storage.js
@@ -0,0 +1,44 @@
+import { Client, ID, Storage } from "appwrite";
+import config from "../config/config.js";
+
+export class StorageService {
+  client = new Client();
+  storage;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appWriteEndPoint)
+      .setProject(config.appWriteProjectId);
+    this.storage = new Storage(this.client);
+  }
+
+  async uploadFile(file) {
+    try {
+      return await this.storage.createFile(
+        config.appWriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: uploadFile :: error", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.storage.deleteFile(config.appWriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite serive :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.storage.getFilePreview(config.appWriteBucketId, fileId);
+  }
+}
+
+const storageService = new StorageService();
+export default storageService;
