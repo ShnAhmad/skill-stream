b3fef4b6340cf1d6fe03b84ff8e3c9eaaefe55e3
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-11 03:59:55 +0500

    routes update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,69 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout";
 import Home from "../pages/Home";
-
+import LoginPage from "../pages/Login";
+import SignupPage from "../pages/Signup";
+import AllPosts from "../pages/AllPosts";
+import AddPost from "../pages/AddPost";
+import EditPost from "../pages/EditPost";
+import Post from "../pages/Post";
+import AuthLayout from "../components/AuthLayout";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children:[
+    children: [
         {
-            path:"/",
-            element:<Home/>
-        }
-    ]
-  },
-]);
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <LoginPage />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <SignupPage />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
