b150657ab52de05ef2375b9ca3bc607bcf693d61
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-11 03:58:45 +0500

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import postService from "../appwrite/post";
+import Container from "../components/Container";
+import PostCard from "../components/PostCard";
+import { useSelector } from "react-redux";
 
-const Home = () => {
+function Home() {
+  const [posts, setPosts] = useState([]);
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      postService.getPosts().then((posts) => {
+        if (posts) {
+          setPosts(posts.documents);
+        }
+      });
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  if (isLoggedIn && posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <h1 className="text-2xl font-bold text-gray-700">
+            No posts yet, be the first to post!
+          </h1>
+        </Container>
+      </div>
+    );
+  }
   return (
-    <div>Home</div>
-  )
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
