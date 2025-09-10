83476956aa932ba4798f85f77eb48f6d80541d67
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-11 00:21:17 +0500

    reuseable button and input

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+ const Button =({
+    children,
+    className = "",
+    ...props
+}) =>{
+    return (
+        <button className={`px-4 py-2 rounded-lg bg-blue-600 text-white ${className}`} {...props}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
