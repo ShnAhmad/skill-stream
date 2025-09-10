303866dd8ffbcf03467e79f53c0c33bd2950e61b
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-11 03:59:28 +0500

    Login and sign up page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from '../components/Signup'
+function LoginPage() {
+  return (
+    <div className='py-8'>
+        <Login />
+    </div>
+  )
+}
+
+export default LoginPage
