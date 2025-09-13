0921f9c0dd319b365c463da64b3348462e5d966c
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-14 02:05:48 +0500

    Home page update

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const AboutSectionContent = [
+    {
+        title: "Sign Up & Login",
+        description:"Create your account to start sharing your coding experiences."
+    },
+    {
+        title: "Add Your Journey",
+        description:"Write posts about your projects, challenges, or coding tips."
+    },
+    {
+        title: "Explore & Learn",
+        description:"Read posts from other developers and get inspired."
+    },
+    {
+        title: "Engage & Collaborate",
+        description:"Comment, share ideas, and grow with the community."
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import postService from "../appwrite/post";
 import PostCard from "../components/PostCard";
 import { useSelector } from "react-redux";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -15,43 +17,16 @@ function Home() {
         } else {
           setPosts([]);
         }
-
       });
     }
   }, [isLoggedIn]);
 
-  if (!isLoggedIn) {
-    return (
-      <div className="container mx-auto py-8 mt-4 text-center">
-          <div className="flex flex-wrap">
-            <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-gray-500">
-                Login to read posts
-              </h1>
-            </div>
-          </div>
-      </div>
-    );
-  }
-  if (isLoggedIn && posts.length === 0) {
-    return (
-      <div className="w-full py-8 mt-4 text-center">
-          <h1 className="text-2xl font-bold text-gray-700">
-            No posts yet, be the first to post!
-          </h1>
-      </div>
-    );
-  }
   return (
-    <div className="w-full py-8">
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard {...post} />
-            </div>
-          ))}
-        </div>
-    </div>
+    <>
+      {/* Hero always visible */}
+      <HeroSection />
+      <AboutSection/>
+    </>
   );
 }
 
