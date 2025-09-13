a5cd104c1cad4b34dcb73146b6166abe50f0682c
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-14 02:06:07 +0500

    Layout update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/header/header'
-import Footer from './components/Footer/Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/header/header";
+import Footer from "./components/Footer/Footer";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen bg-gray-400">
-      <div className="sticky top-0 z-20">
-        <Header />
-      </div>
-
-      <main className="min-h-screen">
-        <Outlet />
+    <div className="min-h-screen">
+      <Header />
+      <main className="relative bg-neutral-900">
+        <div
+          className="absolute inset-0 bg-center bg-repeat opacity-80 mix-blend-multiply"
+          style={{ backgroundImage: "url('/images/BackgroundPattern.png')" }}
+        />
+        <div className="relative z-10">
+          <Outlet />
+        </div>
       </main>
-
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
