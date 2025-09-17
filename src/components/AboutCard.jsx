bedfe9342c594768630a004a82d01dcd89c1f1f8
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-18 03:11:32 +0500

    fixed about card

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const AboutCard = ({title, description}) => {
+const AboutCard = ({ title, description }) => {
   return (
-<div className="bg-[var(--color-secondary-900)] border border-[var(--color-secondary-950)] rounded-lg shadow-lg p-6 flex flex-col items-center transition-transform hover:-translate-y-1 hover:shadow-2xl">
-          <span className="text-primary text-4xl mb-4">
-            <i className="fas fa-user-plus"></i>
-          </span>
-          <h3 className="font-semibold text-xl mb-2">{title}</h3>
-          <p className="text-center">
-            {description}
-          </p>
-        </div>  )
-}
+    <div className="bg-[var(--color-secondary-900)] border border-[var(--color-secondary-950)] rounded-lg shadow-lg p-6 flex flex-col items-center transition-transform hover:-translate-y-1 hover:shadow-2xl">
+      <span className="text-primary text-4xl mb-4">
+        <i className="fas fa-user-plus"></i>
+      </span>
+      <h3 className="font-semibold text-xl mb-2">{title}</h3>
+      <p className="text-center">{description}</p>
+    </div>
+  );
+};
 
-export default AboutCard
+export default AboutCard;
