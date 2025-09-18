a0597d1c0ab76ab892de22906067a48a5496fbd6
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-09-19 04:03:10 +0500

    post card update

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import storageService from '../appwrite/storage';
-import { Link } from 'react-router-dom';
+import React from "react";
+import storageService from "../appwrite/storage";
+import { Link } from "react-router-dom";
 
-const PostCard = ({$id, title, featuredImage}) => {
- let imageUrl = storageService.getFileView(featuredImage);
+const PostCard = ({ $id, title, featuredImage, userName, $createdAt }) => {
+  let imageUrl = storageService.getFileView(featuredImage);
 
   return (
-     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-[var(--color-secondary-900)] border border-[var(--color-secondary-950)] rounded-xl p-4 transition-transform hover:-translate-y-1 hover:shadow-2xl'>
-            <div className='w-full h-58 justify-center mb-4'>
-                <img src={imageUrl} alt={title}
-                className='w-full h-full rounded-xl object-cover' />
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-[var(--color-secondary-900)] border border-[var(--color-secondary-950)] rounded-xl p-4 transition-transform hover:-translate-y-1 hover:shadow-2xl">
+        <div className="w-full h-58 justify-center mb-4">
+          <img
+            src={imageUrl}
+            alt={title}
+            className="w-full h-full rounded-xl object-cover"
+          />
         </div>
+         <h4 className="text-lg font-semibold font-secondary">{title}</h4>
+        <div className="flex items-center gap-3 mt-3 font-secondary text-sm">
+          <div className="flex items-center gap-2">
+            <img
+              src="/images/user.png"
+              className="w-7 h-7 rounded-full object-cover"
+            />
+            <span >{userName}</span>
+          </div>
+          <p>{new Date($createdAt).toLocaleDateString()}</p>
+        </div>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
