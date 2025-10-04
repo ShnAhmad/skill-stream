c101f5c7746cf62d80efe29dd37d4cbde08906e3
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-10-05 01:28:09 +0500

    update edit post screen

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,32 +1,35 @@
-import React, {useEffect, useState} from 'react'
-import { useNavigate,  useParams } from 'react-router-dom';
-import postService from '../appwrite/post';
-import Container from '../components/Container';
-import PostForm from '../components/Post-Form/PostForm';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import postService from "../appwrite/post";
+import Container from "../components/Container";
+import PostForm from "../components/Post-Form/PostForm";
 
 function EditPost() {
-    const [post, setPosts] = useState(null)
-    const {slug} = useParams()
-    const navigate = useNavigate()
+  const [post, setPosts] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (slug) {
-            postService.getPost(slug).then((post) => {
-                if (post) {
-                    setPosts(post)
-                }
-            })
-        } else {
-            navigate('/')
+  useEffect(() => {
+    if (slug) {
+      postService.getPost(slug).then((post) => {
+        if (post) {
+          setPosts(post);
         }
-    }, [slug, navigate])
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
   return post ? (
-    <div className='py-8'>
-        <Container>
-            <PostForm post={post} />
-        </Container>
+    <div className="w-full h-full">
+      <div className="container mx-auto max-w-[1276px] px-4 md:px-6 py-12 md:py-16 lg:py-24">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 items-center">
+          Edit Post
+        </h1>
+        <PostForm post={post} />
+      </div>
     </div>
-  ) : null
+  ) : null;
 }
 
-export default EditPost
+export default EditPost;
