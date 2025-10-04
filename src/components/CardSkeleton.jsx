882c6a1ff9382e38180f60f515ab48658864e039
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-10-05 03:04:09 +0500

    update card skeleton

--- src/components/CardSkeleton.jsx
+++ src/components/CardSkeleton.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CardSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {[...Array(4)].map((_, i) => (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {[...Array(8)].map((_, i) => (
         <div
           key={i}
           className="animate-pulse bg-[var(--color-secondary-900)] h-40 rounded-xl"
