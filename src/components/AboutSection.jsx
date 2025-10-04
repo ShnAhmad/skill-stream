24f62785f1b2e6ffc5a24d317dd842df21932e19
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-10-05 03:54:22 +0500

    fix card styling

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,7 +4,7 @@ import AboutCard from "./AboutCard";
 
 const AboutSection = () => {
   return (
-    <section className="py-12 bg-bg text-text">
+    <section className="px-4 md:px-6 py-12 md:py-16 lg:py-24 bg-bg text-text">
       <div className="max-w-3xl mx-auto text-center">
         <h2 className="text-3xl font-bold mb-2 text-primary">
           How Skill Stream Works
