e604d411b3d92010bb1de8d4ab95bd87416f4f0a
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-10-05 01:07:11 +0500

    add post styling update

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import PostForm from '../components/Post-Form/PostForm'
+import React from "react";
+import PostForm from "../components/Post-Form/PostForm";
 
 function AddPost() {
   return (
-    <div className='w-full h-full'>
-            <PostForm />
+    <div className="w-full h-full">
+      <div className="container mx-auto max-w-[1276px] px-4 md:px-6 py-12 md:py-16 lg:py-24">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 items-center">
+          Add New Post
+        </h1>
+        <PostForm />
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddPost
+export default AddPost;
